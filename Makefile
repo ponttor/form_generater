9d601b712eb44193f0562f686619ff0ffd2b9d8f
Alexander Illarionov <alex@heimstudio.ru>
2022-02-17 17:45:36 +0100

    generate gem

---
+++ Makefile
@@ -0,0 +1,9 @@
+install:
+	bundle install
+
+test:
+	bundle exec rake test
+
+lint:
+	bundle exec rake rubocop
+
---
+++ lib/hexlet_code.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'hexlet_code/version'
+
+module HexletCode
+  class Error < StandardError; end
+
+end
+
