600267229acfd3d4b137e07927b575595188f100
Alexander Illarionov <alex@heimstudio.ru>
2022-02-17 17:46:51 +0100

    create Tag

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,5 +5,15 @@ require_relative 'hexlet_code/version'
 module HexletCode
   class Error < StandardError; end
 
+  class Tag
+    def self.build(name, content)
+      contents = []
+      content.each do |type, value|
+        contents.push(%(#{type}=\"#{value}\"))
+      end
+      %(<#{name} #{contents.join(' ')}>)
+    end
+  end
 end
 
+p HexletCode::Tag.build('br', a: 'b', c: 'd')
