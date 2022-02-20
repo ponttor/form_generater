03d3ec4337d84e7c24a3c985b5155bd819826c6f
Alexander Illarionov <alex@heimstudio.ru>
2022-02-20 18:14:57 +0100

    add tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  # def test_it_does_something_useful
+  #   assert false
+  # end
+
+  def test_build_single_tag
+    assert(HexletCode::Tag.build('br') == '<br>')
+  end
+
+  def test_build_single_tag_with_content
+    assert(HexletCode::Tag.build('img', src: 'path/to/image') == '<img src="path/to/image">')
+  end
+
+  def test_build_single_tag_with_double_content
+    assert(HexletCode::Tag.build('input', type: 'submit', value: 'Save') == '<input type="submit" value="Save">')
+  end
+
+  def test_build_double_tag
+    assert(HexletCode::Tag.build('label') { 'Email' } == '<label>Email</label>')
+  end
+
+  def test_build_double_tag_with_content
+    assert(HexletCode::Tag.build('label', for: 'email') { 'Email' } == '<label for="email">Email</label>')
+  end
+
+  def test_build_double_tag_empty
+    assert(HexletCode::Tag.build('div') == '<div></div>')
   end
 end
