546845422302d88666319943c1b15dfa5ea0cb97
Alexander Illarionov <alex@heimstudio.ru>
2022-02-26 16:43:51 +0100

    create Tag (#1)
    * create Tag
    
    * build without hash parameter
    
    * class Tag fix
    
    * add tests
    
    * test_hexlet
    
    * fix conditions
    
    Co-authored-by: Alexander <alexander@iMac-Alexander.local>

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
+require_relative 'hexlet_code/tag'
 
 module HexletCode
   class Error < StandardError; end
-
 end
-
---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module HexletCode
+  class Tag 
+    def self.build(name, options = {})
+
+      # def stringify_tags(name, options = {})
+      #   contents = options.map do |type, value|
+      #     %(#{type}="#{value}")
+      #   end
+      #   %( #{contents.join(' ')})
+      # end
+
+      single_tags = %w[br img input]
+
+      tag_contents = options.map do |type, value|
+        %(#{type}="#{value}")
+      end
+      
+      content = block_given? ? yield : ''
+      tags_string = %( #{tag_contents.join(' ')})
+ 
+      if single_tags.include?(name)
+        return %(<#{name}>) if tags_string == ' '
+        %(<#{name}#{tags_string}>)
+      end
+
+      return %(<#{name}>#{content}</#{name}>) if tags_string == ' '
+      %(<#{name}#{tags_string}>#{content}</#{name}>)
+      
+    end
+  end
+end
