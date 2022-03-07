303699ca89f499006e64eda1b9ba77a084a6dae5
Alexander Illarionov <alex@heimstudio.ru>
2022-03-07 16:09:57 +0100

    rubocop fix

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -4,17 +4,6 @@ require 'test_helper'
 require 'power_assert/colorize'
 
 class TestHexletCode < Minitest::Test
-
-  # User = Struct.new(:name, :job, keyword_init: true)
-
-  # def test_form_for
-  #   user = User.new name: 'rob', job: 'hexlet', gender: 'm'
-  #   actual = HexletCode.form_for user do |f|
-  #   end 
-  #   expected = '<form action="#" method="post"></form>'
-  #   assert { actual == expected }
-  # end
-
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
@@ -42,4 +31,14 @@ class TestHexletCode < Minitest::Test
   def test_build_double_tag_empty
     assert { HexletCode::Tag.build('div') == '<div></div>' }
   end
+
+  # User = Struct.new(:name, :job, keyword_init: true)
+
+  # def test_form_for
+  #   user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+  #   actual = HexletCode.form_for user do |f|
+  #   end
+  #   expected = '<form action="#" method="post"></form>'
+  #   assert { actual == expected }
+  # end
 end
