a6c82b41679f195ddcf5c418e19a574f0683ecf7
Alexander Illarionov <alex@heimstudio.ru>
2022-03-18 18:54:39 +0100

    tag options

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -10,12 +10,20 @@ module HexletCode
 
     def input(param, as_param = {})
       @contents.push(Tag.build('label', { for: param.to_s }) { param.capitalize.to_s })
-      if as_param != {}
-        @contents.push(Tag.build('textarea', { cols: '20', rows: '40', name: param.to_s }) do
+
+      tag_rules = { :text => 'textarea' }
+
+      if !as_param.empty?
+        as = as_param.select {|key, value| key == :as}
+        tag_options = as_param.select {|key, value| key != :as}
+        tag_default = { cols: '20', rows: '40', name: param.to_s }
+        tag_options_all = tag_default.merge(tag_options)
+        tag_final = as.empty? ? 'input' : tag_rules[as[:as]]
+        @contents.push(Tag.build(tag_final, tag_options_all) do
           @user.job.to_s
         end)
       end
-      @contents.push(Tag.build('input', { name: param, type: 'text', value: @user[param] })) if as_param == {}
+      @contents.push(Tag.build('input', { name: param, type: 'text', value: @user[param] })) if as_param.empty?
     end
 
     def submit(param = 'save')
