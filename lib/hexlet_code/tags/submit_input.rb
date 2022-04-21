44d08993e2ff6ec4a235b2738cf75dbcc27c42f4
Alexander Illarionov <alex@heimstudio.ru>
2022-04-21 22:01:57 +0200

    submit block

--- lib/hexlet_code/tags/submit_input.rb
+++ lib/hexlet_code/tags/submit_input.rb
@@ -3,10 +3,10 @@
 module HexletCode
   module Tags
     class SubmitInput
-      def self.build(_element)
+      def self.build(element)
         tag = 'input'
         options = { name: 'commit', type: 'submit', value: 'Save' }
-        block = ''
+        block = element || ''
 
         { tag: tag, options: options, block: block }
       end
